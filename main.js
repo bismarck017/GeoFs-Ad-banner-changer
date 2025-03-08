1b5bcb534282a6b435fef46946ecd3c9b906b0ed
Matt <matthewsantos2403@gmail.com>
2025-03-08 13:07:50 -0500

    updated to combat adblckers

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
-const targetDiv = document.querySelector('.geofs-refreshTarget');
+function ensureTargetVisible() {
+    const targetDiv = document.querySelector('.geofs-refreshTarget');
+    const adBlock = document.querySelector('.ads.adsbox.adBlock');
 
-if (targetDiv) {
-    targetDiv.style.display = "block";
-    targetDiv.style.visibility = "visible";
+    if (adBlock) {
+        adBlock.style.display = "none";
+    }
 
-    targetDiv.innerHTML = `
-        <div style="font-family: sans-serif; font-size: 11px; padding: 20px;">
-            <h6>Fox-2:</h6>
-            <ul>
-                <li>AIM-9 Sidewinder</li>
-                <li>ASRAAM / AIM-132</li>
-                <li>IRIS-T</li>
-                <li>AAM-3</li>
-                <li>Bozdoğan / Merlin</li>
-                <li>Rafael Python 5</li>
-                <li>Matra Magic II</li>
-                <li>Matra R.510</li>
-                <li>Matra R.530</li>
-                <li>MAA-1A Piranha</li>
-                <li>MAA-1B Piranha</li>
-                <li>MBDA MICA IR</li>
-                <li>PL-9</li>
-                <li>R-60 / AA-8 Aphid</li>
-                <li>R-27T / AA-10T Alamo</li>
-                <li>Sky Sword 1 / TC-1</li>
-            </ul>
-        </div>
-    `;
-} else {
-    console.error("Element with class 'geofs-refreshTarget' not found.");
+    if (targetDiv) {
+        targetDiv.style.display = "block";
+        targetDiv.style.visibility = "visible";
+
+        targetDiv.innerHTML = `
+            <div style="font-family: sans-serif; font-size: 11px; padding: 20px;">
+                <h6>Fox-2:</h6>
+                <ul>
+                    <li>AIM-9 Sidewinder</li>
+                    <li>ASRAAM / AIM-132</li>
+                    <li>IRIS-T</li>
+                    <li>AAM-3</li>
+                    <li>Bozdoğan / Merlin</li>
+                    <li>Rafael Python 5</li>
+                    <li>Matra Magic II</li>
+                    <li>Matra R.510</li>
+                    <li>Matra R.530</li>
+                    <li>MAA-1A Piranha</li>
+                    <li>MAA-1B Piranha</li>
+                    <li>MBDA MICA IR</li>
+                    <li>PL-9</li>
+                    <li>R-60 / AA-8 Aphid</li>
+                    <li>R-27T / AA-10T Alamo</li>
+                    <li>Sky Sword 1 / TC-1</li>
+                </ul>
+            </div>
+        `;
+    } else {
+        console.error("Element with class 'geofs-refreshTarget' not found.");
+    }
 }
+
+ensureTargetVisible();
+
+setInterval(ensureTargetVisible, 2000);
